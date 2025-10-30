031747315bad9fb1f3eea47226392bc5ab80e104
Taehoon Kim <taeh.kim.n@gmail.com>
2025-10-30 04:47:31 -0400

    deploying pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://docs.astro.build/en/reference/configuration-reference/
 export default defineConfig({
-  site: 'https://example.github.io/', // set this later for sitemap, etc.
+  site: 'https://taehkim1280.github.io/', // set this later for sitemap, etc.
   scopedStyleStrategy: 'where',
 });
